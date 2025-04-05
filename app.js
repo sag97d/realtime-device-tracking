64239066076d76fe27a9b3c7feed64c9cbb79518
sag97d <131565578+sag97d@users.noreply.github.com>
2025-04-05 14:40:54 +0530

    fourth-commit

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ const io = socketio(server)
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, "public")));
 
+const users = {};
+
+
 io.on("connection", (socket) => {
+    socket.emit("existing-users", users);
+
     socket.on("send-location", (data) => {
+        users[socket.id] = data;
         io.emit("receive-location", { id: socket.id, ...data })
     })
     socket.on("disconnect", ()=>{
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 const socket = io()
+const username = prompt("Enter your username") || "Anonymous";
 
 navigator.permissions.query({ name: "geolocation" }).then((result) => {
     if (result.state === "denied") {
@@ -10,7 +11,7 @@ navigator.permissions.query({ name: "geolocation" }).then((result) => {
 if (navigator.geolocation) {
     navigator.geolocation.watchPosition((position) => {
         const { latitude, longitude } = position.coords;
-        socket.emit('send-location', { latitude, longitude })
+        socket.emit('send-location', { username, latitude, longitude })
     }, (error) => {
         console.error(error);
     },
@@ -29,21 +30,86 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "@Sag maps"
 }).addTo(map)
 
+// Flag to know when we’ve sent our location
+let hasSentOwnLocation = false;
+let pendingUsers = null;
+
+function addMarker(id, latitude, longitude, username = "Unknown") {
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+        // markers[id] = L.marker([latitude, longitude]).addTo(map);
+        const marker = L.marker([latitude, longitude]).addTo(map);
+        marker.bindPopup(`<b>${username}</b>`).openPopup(); 
+        markers[id] = marker;
+    }
+}
+
 const markers = {}
 
+// socket.on("existing-users", (users) => {
+//     for (let id in users) {
+//         const { latitude, longitude } = users[id];
+//         if (id !== socket.id) { // skip your own marker if you're already handling it
+//             markers[id] = L.marker([latitude, longitude]).addTo(map);
+//         }
+//     }
+// });
+
+socket.on("existing-users", (users) => {
+    if (hasSentOwnLocation) {
+        for (let id in users) {
+            if (id !== socket.id) {
+                const { latitude, longitude, username } = users[id];
+                addMarker(id, latitude, longitude, username);
+            }
+        }
+    } else {
+        pendingUsers = users;
+    }
+});
+
+
+// ✅ 1. First get and send my location
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit('send-location', { latitude, longitude });
+
+        // Show my own marker
+        addMarker(socket.id, latitude, longitude, username);
+        map.setView([latitude, longitude]);
+
+        hasSentOwnLocation = true;
+
+        // 🔁 If existing users came in before we sent location, process now
+        if (pendingUsers) {
+            for (let id in pendingUsers) {
+                if (id !== socket.id) {
+                    const { latitude, longitude } = pendingUsers[id];
+                    addMarker(id, latitude, longitude);
+                }
+            }
+            pendingUsers = null;
+        }
+    });
+}
+
+
 socket.on("receive-location", (data) => {
-    const { id, latitude, longitude } = data;
+    const { id, latitude, longitude, username } = data;
+    addMarker(id, latitude, longitude, username);
     map.setView([latitude, longitude])
-    if(markers[id]){
+    if (markers[id]) {
         markers[id].setLatLng([latitude, longitude])
     }
-    else{
+    else {
         markers[id] = L.marker([latitude, longitude]).addTo(map);
     }
 })
 
-socket.on('user-disconnected', (id)=>{
-    if(markers[id]){
+socket.on('user-disconnected', (id) => {
+    if (markers[id]) {
         map.removeLayer(markers[id])
         delete markers[id]
     }
